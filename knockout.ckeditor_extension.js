ae25966e4ff48322fb49d500d8579a014d85225c
Jonathan Cherryhomes <cherryhomesj@gmail.com>
2013-08-03 08:56:03 -0500

    set default toolbar in the case that options are not passed to the extension

--- knockout.ckeditor_extension.js
+++ knockout.ckeditor_extension.js
@@ -1,4 +1,7 @@
 // Requires knockoutjs and CKEditor
+//
+// Credit goes to Skrile on stackoverflow for posting this extension
+// http://stackoverflow.com/a/14688385/345156
 
 ko.bindingHandlers.richText = {
     init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
@@ -6,16 +9,18 @@ ko.bindingHandlers.richText = {
         var options = allBindingsAccessor().richTextOptions || {};
 
         // You can set the toolbar options here
-        options.toolbar_Full = [
-            { name: 'document', items: ['Source', '-', 'NewPage', 'Preview'] },
-            { name: 'clipboard', items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo'] },
-            { name: 'editing', items: ['Find', 'Replace', '-', 'SelectAll', '-', 'Scayt'] },
-            { name: 'insert', items: ['Image', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak'] },
-            { name: 'basicstyles', items: ['Bold', 'Italic', 'Strike', '-', 'RemoveFormat'] },
-            { name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote'] },
-            { name: 'links', items: ['Link', 'Unlink', 'Anchor'] },
-            { name: 'tools', items: ['Maximize', '-', 'About'] }
-        ];
+        if (!options.toolbar){
+            options.toolbar = [
+                { name: 'document', items: ['Source', '-', 'NewPage', 'Preview'] },
+                { name: 'clipboard', items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo'] },
+                { name: 'editing', items: ['Find', 'Replace', '-', 'SelectAll', '-', 'Scayt'] },
+                { name: 'insert', items: ['Image', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak'] },
+                { name: 'basicstyles', items: ['Bold', 'Italic', 'Strike', '-', 'RemoveFormat'] },
+                { name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote'] },
+                { name: 'links', items: ['Link', 'Unlink', 'Anchor'] },
+                { name: 'tools', items: ['Maximize', '-', 'About'] }
+            ];
+        }
 
         //handle disposal (if KO removes by the template binding)
         ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
@@ -23,6 +28,19 @@ ko.bindingHandlers.richText = {
         });
 
         $(element).ckeditor(options);
+
+        //handle edits made in the editor
+        CKEDITOR.instances.thread_message.on('contentDom', function() {
+          CKEDITOR.instances.thread_message.document.on('keyup', function(e) {
+
+            var self = this;
+            if (ko.isWriteableObservable(self)) {
+              var ckValue = CKEDITOR.instances.element_id.getData();
+              self(ckValue);
+              //console.log("value: " + ckValue);
+            }
+          }, modelValue, element);
+        });
 
         //wire up the blur event to ensure our observable is properly updated
         CKEDITOR.instances[txtBoxID].focusManager.blur = function () {
