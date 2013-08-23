8896a52f498c034ba59f21d1845e86ae8632c8f9
Jonathan Cherryhomes <cherryhomesj@gmail.com>
2013-08-22 22:41:28 -0500

    added popover to subtext extension
    updated subtext extension to apply a popover to the subtext field if the
    text is longer than the specified max length. Requires bootstrap.js

--- knockout.subtext_extension.js
+++ knockout.subtext_extension.js
@@ -1,7 +1,7 @@
-// Requires Knockout.js and Moment.js
+// Requires Knockout.js and Bootstrap.js
 
 // Knockoutjs: http://knockoutjs.com/
-// Momentjs: http://momentjs.com/
+// Bootstrap: http://getbootstrap.com
 
 ko.bindingHandlers.subtext = {
     init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
@@ -18,12 +18,15 @@ ko.bindingHandlers.subtext = {
             bindingsAccessor = allBindingsAccessor();
 
         var maxLength = bindingsAccessor.maxLength || 20;
+        var trigger = bindingsAccessor.trigger || 'hover';
+        var placement = bindingsAccessor.placement || 'bottom';
 
         if (val != null && val != ''){
             if (val.length > maxLength) {
                 val = val.substr(0, maxLength-3) + '...';
+                $(element).popover({trigger: trigger, placement: placement})
             }
-
+            
             $(element).text(val);
         }
     }
